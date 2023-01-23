e20ff90342d63a1c892b94aac1211cec948f074f
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-01-23 20:27:42 +0000

    complete exercise 4 and 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(numA,numB) {
+    let finalSum = 0;
+    let error = "ERROR";
+    let largerValue = Math.max(numA,numB);
+    let smallerValue = Math.min(numA,numB);
+    if (((typeof numA==="string"))||(typeof numB==="string")){
+        return error;
+    } else if (((typeof numA==="object"))||(typeof numB==="object")){
+        return error;
+    } else if ((largerValue >= 0)&&(smallerValue >=0)){
+        for (let i = smallerValue; i<=largerValue; i++ ){
+            finalSum = finalSum+i;
+        } 
+    } else return error 
+    return finalSum;
+    
+}
 
 // Do not edit below this line
 module.exports = sumAll;
