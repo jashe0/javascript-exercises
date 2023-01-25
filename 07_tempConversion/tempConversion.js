45ceb43f946ad99ab0d25472a2d056b595868723
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-01-25 14:39:42 +0000

    finish exercises 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const fTemp = fahrenheit;
+  const toCels = (fTemp - 32) *5/9;
+  let result = Math.round(toCels*10)/10
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const cTemp = celsius;
+  const toFahr = cTemp * 9/5 +32;
+  let result = Math.round(toFahr*10)/10
+  return result; 
 };
 
 // Do not edit below this line
