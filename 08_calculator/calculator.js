2dbd283e16bd720baa490af6d638d9e0ec51c9bd
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-02-01 20:25:51 +0000

    compete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(numA,numB) {
+	let numC = numA + numB;
+  return numC;
 };
 
-const subtract = function() {
-	
+const subtract = function(numA,numB) {
+  let numC = numA - numB;
+  return numC;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if(!Array.isArray(arr)) return;
+  let totalNumber = 0;
+  for (let i=0,l=arr.length; i<l; i++) {
+     totalNumber+=arr[i];
+  }
+  return totalNumber;
+
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let output = 1;
+  for (let i = 0; i < arr.length; ++i) { 
+    output *= arr[i]; 
+  } 
+  return output;
 };
 
-const power = function() {
-	
+const power = function(numA,numB) {
+	let power = Math.pow(numA,numB);
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+    else {
+        return (num * factorial(num - 1));
+    }	
 };
 
 // Do not edit below this line
